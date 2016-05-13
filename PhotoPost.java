fe1349551c87b4424618d1c95e9c01a7c80d6760
DavidG <igdrasilenllamas@gmail.com>
2016-05-13 12:15:13 +0200

    Todos los post se manejan como lo mismo con diferentes carazteristicas

--- PhotoPost.java
+++ PhotoPost.java
@@ -6,51 +6,19 @@ import java.util.ArrayList;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class PhotoPost
+public class PhotoPost extends Post
 {
-    private String username;
     private String filename;
     private String caption;
-    private long timestamp;
-    private int likes;
-    private ArrayList<String> comments;
 
     /**
      * Constructor for objects of class PhotoPost
      */
     public PhotoPost(String author, String filename, String caption)
     {
-        username = author;
+        super(author);
         this.filename = filename;
         this.caption = caption;
-        timestamp = System.currentTimeMillis();
-        likes = 0;
-        comments = new ArrayList<>();
-    }
-
-    /**
-     * Add a like
-     *
-     */
-    public void like()
-    {
-        likes++;
-    }
-
-    /**
-     * Drop a like
-     *
-     */
-    public void unlike()
-    {
-        if(likes > 0){likes--;}
-    }
-
-    /**
-     * Add a comment of the user
-     */
-    public void addComment(String text){
-        comments.add(text);
     }
 
     /**
@@ -65,52 +33,5 @@ public class PhotoPost
      */
     public String getCaption(){
         return caption;
-    }
-
-    /**
-     * @return the number to indicate the moment
-     */
-    public long getTimeStamp(){
-        return timestamp;
-    }
-
-    /**
-     * Print the post
-     */
-    public void display(){
-        System.out.println("Autor: " + username);
-        System.out.println();
-        System.out.println(filename);
-        System.out.println();
-        System.out.println(caption);
-        System.out.println(); 
-        System.out.println(timeString(timestamp));
-        System.out.println("Nº de likes: " + likes + ".");
-        System.out.println();
-        System.out.println("---------------- Comentarios --------------------");
-        if(comments.isEmpty()){
-            System.out.println("Este post no tiene ningún comentario de momento.");
-        }
-        else{
-            for(String comment : comments){
-                System.out.println(comment);
-                System.out.println();
-            }
-        }
-        System.out.println("###############################################################");
-        System.out.println("###############################################################");
-        System.out.println();
-        System.out.println();
-    }
-
-    /**
-     * @return devuelve los milisegundos en timeS
-     */
-    private String timeString(long time){
-        long count = System.currentTimeMillis() - time;
-        long totalSec = count / 1000;
-        long min = totalSec / 60;
-        long sec = totalSec % 60;
-        return"Se posteo hace: " + min + " minutos y " + sec + " segundos.";        
     }
 }
---
+++ Post.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Post here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Post
+{
+    private String username;
+    private long timestamp;
+    private int likes;
+    private ArrayList<String> comments;    
+
+    /**
+     * Constructor for objects of class MessagePost
+     */
+    public Post(String author)
+    {
+        username = author;
+        timestamp = System.currentTimeMillis();
+        likes = 0;
+        comments = new ArrayList<>();
+    }
+    
+    /**
+     * @return username
+     */
+    public String getUsername(){
+        return username;
+    }
+
+    /**
+     * Add a like
+     *
+     */
+    public void like()
+    {
+        likes++;
+    }
+
+    /**
+     * Drop a like
+     *
+     */
+    public void unlike()
+    {
+        if(likes > 0){likes--;}
+    }
+
+    /**
+     * Add a comment of the user
+     */
+    public void addComment(String text){
+        comments.add(text);
+    }
+
+    /**
+     * @return the number to indicate the moment
+     */
+    public long getTimeStamp(){
+        return timestamp;
+    }
+
+    /**
+     * Print the post
+     */
+    public void display(){
+        System.out.println("Autor: " + username);
+        System.out.println();
+        System.out.println(); 
+        System.out.println(timeString(timestamp));
+        System.out.println("Nº de likes: " + likes + ".");
+        System.out.println();
+        System.out.println("---------------- Comentarios --------------------");
+        if(comments.isEmpty()){
+            System.out.println("Este post no tiene ningún comentario de momento.");
+        }
+        else{
+            for(String comment : comments){
+                System.out.println(comment);
+                System.out.println();
+            }
+        }
+        System.out.println("###############################################################");
+        System.out.println("###############################################################");
+        System.out.println();
+        System.out.println();
+    }
+    
+    /**
+     * @return devuelve los milisegundos en timeS
+     */
+    private String timeString(long time){
+        long count = System.currentTimeMillis() - time;
+        long totalSec = count / 1000;
+        long min = totalSec / 60;
+        long sec = totalSec % 60;
+        return"Se posteo hace: " + min + " minutos y " + sec + " segundos.";        
+    }
+}
+
