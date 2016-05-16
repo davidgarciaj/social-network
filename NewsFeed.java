10c0789c7712aea00a755699474d3916347e8116
DavidG <igdrasilenllamas@gmail.com>
2016-05-16 13:05:49 +0200

    refactorizado newsfeed

--- NewsFeed.java
+++ NewsFeed.java
@@ -8,18 +8,14 @@ import java.util.ArrayList;
  */
 public class NewsFeed
 {
-    private ArrayList<MessagePost> messages;
-    private ArrayList<PhotoPost> photos;
-    private ArrayList<JoinGroupPost> joinGroups;
+    private ArrayList<Post> posts;
 
     /**
      * Constructor for objects of class NewsFeed
      */
     public NewsFeed()
     {
-        messages = new ArrayList<>();
-        photos = new ArrayList<>();
-        joinGroups = new ArrayList<>();
+        posts = new ArrayList<>();
     }
 
     /**
@@ -27,43 +23,17 @@ public class NewsFeed
      * 
      * @message the message to introduce
      */
-    public void addMessagePost(MessagePost message)
+    public void addPost(Post post)
     {
-        messages.add(message);
-    }
-
-    /**
-     * Add a post with a image
-     * 
-     * @message the image to introduce
-     */
-    public void addPhotoPost(PhotoPost photo)
-    {
-        photos.add(photo);
-    }
-
-    /**
-     * Add a post with a image
-     * 
-     * @message the image to introduce
-     */
-    public void addJoinGroupPost(JoinGroupPost join)
-    {
-        joinGroups.add(join);
+        posts.add(post);
     }
     
     /**
      * Show the post
      */
     public void show(){
-        for(MessagePost message : messages){
-            message.display();
-        }
-        for(PhotoPost photo : photos){
-            photo.display();
-        }
-        for(JoinGroupPost joinGroup : joinGroups){
-            joinGroup.display();
+        for(Post post : posts){
+            post.display();
         }
     }
 }
